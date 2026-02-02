2813d8e123d3b5312ab854dfe6f04893d8fb13f0
nine-pixel431 <nainamishra1222@gmail.com>
2026-02-02 06:33:39 +0530

    Update 1.1 Basic Mathematical Operations.py

--- 1.1 Basic Mathematical Operations.py
+++ 1.1 Basic Mathematical Operations.py
@@ -4,8 +4,14 @@ num2=int(input("Enter second number: "))
 add=num1+num2
 sub=num1-num2
 mul=num1*num2
-div=num1/num2
+
+if num2 != 0:
+    div= num1 / num2
+else:
+    div= "Undefined (division by zero)"
+
 print("Addition:", add)
 print("Subtraction:", sub)
 print("Multiplication:", mul)
 print("Division:", div)
+
